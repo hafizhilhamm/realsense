defc5eafe7e7a3a1d89c14cec91e56c83ec117fb
hafizhilhamm <hafizhilham36@gmail.com>
2023-02-14 10:44:02 +0700

    lmao

---
+++ CMakeFiles/realsense.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/realsense.dir/test.cpp.o"
+  "realsense"
+  "realsense.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/realsense.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
